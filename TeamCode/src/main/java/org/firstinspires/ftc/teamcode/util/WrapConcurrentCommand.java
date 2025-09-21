7ba43fddd8d76e60c4451845d539e49c3d2f2f41
Connor Larson <absolutelynotahuman@gmail.com>
2025-09-21 10:42:10 -0500

    Taught WrapConcurrent... to rely on a predicate.
    This is in contrast to how it relied on a enumerable state. A utility
    method has also been provided to easily allow users to end until the
    state has been changed *from* the given state, not *to* the given state.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WrapConcurrentCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WrapConcurrentCommand.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.util;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.Subsystem;
 
+import java.util.function.BooleanSupplier;
+
 /**
  * Wraps a method that otherwise acts concurrently. For example, 
  * `IntakeSubsytem.pivotToIntake()` normally needs to only be called once, 
@@ -14,13 +16,42 @@ import com.arcrobotics.ftclib.command.Subsystem;
  * by user code or throught the `CommandScheduler.run()` method.
  */
 public class WrapConcurrentCommand<E extends Enum> extends CommandBase {
+    /**
+     * Creates a new WrapConcurrentCommand that is finished once the state is *not* 
+     * the given state.
+     * 
+     * @param requirement The subsystem used by the command
+     * @param initialAction What is done at the start of the action only.
+     * @param continutationState The state the subsytem will not be finished in. 
+     * If the state is ever different, the command will be finished.
+     */
+    public static <T extends Enum> WrapConcurrentCommand<T> wrapUntilNotState(
+        StateSubsystem<T> requirement,
+        Runnable initialAction,
+        T continuationState
+    ) {
+        return new WrapConcurrentCommand<T>(
+            requirement, 
+            initialAction, 
+            () -> requirement.getState() != continuationState
+        );
+    }
+    
     private final Runnable initialAction;
     private final StateSubsystem<E> subsystem;
-    private final E endState;
+    private final BooleanSupplier endCondition;
     
     public WrapConcurrentCommand(StateSubsystem<E> requirement, Runnable initialAction, E endState) {
+        this(requirement, initialAction, () -> requirement.getState() == endState);
+    }
+
+    public WrapConcurrentCommand(
+        StateSubsystem<E> requirement, 
+        Runnable initialAction, 
+        BooleanSupplier endCondition
+    ) {
         this.initialAction = initialAction;
-        this.endState = endState;
+        this.endCondition = endCondition;
         this.subsystem = requirement;
         addRequirements((Subsystem) requirement);
     }
@@ -32,6 +63,6 @@ public class WrapConcurrentCommand<E extends Enum> extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return subsystem.getState() == endState;
+        return endCondition.getAsBoolean();
     }
 }
