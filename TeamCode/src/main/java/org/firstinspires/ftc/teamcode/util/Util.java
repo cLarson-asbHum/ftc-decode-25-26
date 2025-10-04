683f6fb720e3d72cab15a526317e1de3ab4c3238
Connor Larson <absolutelynotahuman@gmail.com>
2025-10-04 13:34:36 -0500

    Created Util class for common utility methods.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Util.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public final class Util {
+    public static boolean near(double x, double target, double tolerance) {
+        return Math.abs(x - target) < tolerance;
+    }
+
+    public static double lerp(double a, double t, double b) {
+        return t * (b - a) + a;
+    }
+
+    public static double invLerp(double a, double l, double b) {
+        return (l - a) / (b - a);
+    }
+
+    public static double clamp(double min, double x, double max) {
+        return Math.max(min, Math.min(x, max));
+    }
+
+    public static String header(String title, int length, String filler) {
+        final int leftRepeat = (length - title.length()) / (2 * filler.length());
+        final int rightRepeat = (length - title.length()) / filler.length() - leftRepeat;
+        return filler.repeat(leftRepeat) + title + filler.repeat(rightRepeat); 
+    }
+
+    public static final int DEFAULT_HEADER_LENGTH = 60;
+
+    public static String header(String title, int length) {
+        return header(title, length, "-");
+    }
+
+    public static String header(String title) {
+        return header(title, DEFAULT_HEADER_LENGTH);
+    }
+
+    public static String padHeader(String title, int length, String filler) {
+        return header(" " + title + " ", length, filler);
+    }
+
+    public static String padHeader(String title, int length) {
+        return header(" " + title + " ", length);
+    } 
+    
+    public static String padHeader(String title) {
+        return header(" " + title + " ");
+    } 
+}
