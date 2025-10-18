afaa5998650654850502f7800d17affb27257a19
Connor Larson <absolutelynotahuman@gmail.com>
2025-10-18 07:59:35 -0500

    Introduced Sloth for hot-code reloading.
    KNOWN ISSUE: Panels Configurables support is broken when Sloth is
    installed. An issue is currently open on the Sloth Github (opened by
    someone else). It is possible, however, this is never fixed there, and a
    custom Panels Plugin will be made for our repo (ftc-decode-25-26)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/temp/SlothTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.temp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+@TeleOp(group="B - Testing")
+public final class SlothTest extends LinearOpMode {
+    public static long SLEEP_DUR = 1500; // Milliseconds
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.addLine("Sleeping for " + SLEEP_DUR + " milliseconds");
+        telemetry.update();
+
+        // try {
+            sleep(SLEEP_DUR);
+        // } catch (InterruptedException err) {
+        //     Just interuppted; do nothing.
+        // }
+
+        while(opModeInInit()) {
+            telemetry.addData("Status", "Waiting for start");
+            // telemetry.addLine("Did Something *cool* 🥳🥳🥳!");
+            telemetry.addLine("Did Something *cool🦾🦾🦾🦾!");
+            telemetry.addData("Seconds Since Init", getRuntime());
+            telemetry.update();
+        }
+
+        // waitForStart();
+
+        resetRuntime();
+        while(opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Seconds Since Init", getRuntime());
+            telemetry.update();
+        }
+    }
+}
